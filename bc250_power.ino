cf5ab6c1e7dc388d718cff0067b27fb11ec1dc4c
coolsolgit <toptiptap@gmail.com>
2026-09-10 09:35:47 +0900

    Update bc250_power.ino

--- bc250_power.ino
+++ bc250_power.ino
@@ -31,7 +31,7 @@ void setup() {
 void loop() {
 
   bool stateNow = digitalRead(PIN_NUM_BTN); //현재 버튼눌림상태
-  unsigned long timerNow = millis(); //현재 Arduino UpTime
+  unsigned long timerNow = millis(); //현재 Arduino UpTime MilSec
   
   //버튼눌림상태가 변함
   if (stateNow != stateTemp) {
