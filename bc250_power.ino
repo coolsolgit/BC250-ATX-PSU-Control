e8b2e7a14fb5f3a7d42ee70cfd38f87772d6cda9
coolsolgit <toptiptap@gmail.com>
2026-09-11 10:09:56 +0900

    Update bc250_power.ino

--- bc250_power.ino
+++ bc250_power.ino
@@ -55,7 +55,7 @@ void loop() {
       else {
         // BC250 Linux에서(bazzite chchyOS)에 시리얼포트(ttyUSB0등)에 Listening을 걸면 BC250이 리부팅됨
         // Nano호환보드의 USB칩인 CH320G문제인지 리눅스커널의 CH320드라이버 문제인지 아직 몰라 사용보류
-        Serial.println("SHELL_SHUTDOWN");
+        // Serial.println("SHELL_SHUTDOWN");
       }
     }
     
