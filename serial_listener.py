22471b1587916b1bc37f9b496dd5da6e8ce620e6
coolsolgit <toptiptap@gmail.com>
2026-09-12 08:35:20 +0900

    Clarify comments regarding BC250 serial port issues
    Updated comments to clarify the solution for serial port issues with BC250.

--- serial_listener.py
+++ serial_listener.py
@@ -1,6 +1,7 @@
 # BC250 Linux에서(bazzite chchyOS)에 시리얼포트(ttyUSB0등)에 Listening을 걸면 BC250이 리부팅됨
 # Nano호환보드의 USB칩인 CH320G문제인지 리눅스커널의 CH320드라이버 문제인지 아직 몰라 사용보류
-# 하나의 Arduino로 시리얼포트까지 쓰니 문제가 생김, 결국 별도의 시리얼포트(USB to TTL)을 구매해서 BC250에 장착해서 해결
+# 하나의 Arduino로 시리얼포트까지 쓰니 문제가 생김, 결국 별도의 시리얼포트(USB to TTL)을 구매해서 BC250에 장착하고
+# 이쪽으로 Arduino Tx/Rx/GND를 연결하니 정상동작함
 # poweroff권한없이 호출하려면 권한편집 > sudo nano /etc/sudoers
 # 맨아래줄추가 > username ALL=(ALL) NOPASSWD: /usr/bin/systemctl poweroff, /usr/bin/shutdown
 
